676a6867196d66b79ffa177c16b1822ed2c0f441
wasgton <wasgton.it@gmail.com>
2021-06-04 07:50:28 -0300

    Finalizando app

--- lib/Pages/Home.dart
+++ lib/Pages/Home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,13 +10,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  var _frases = [
+    "frase1",
+    "frase2",
+    "frase3",
+    "frase4",
+    "frase5",
+    "frase6",
+    "frase7",
+    "frase8",
+    "frase9",
+    "frase10",
+    "frase11",
+    "frase12",
+    "frase13",
+    "frase14",
+    "frase15",
+    "frase16",
+    "frase17",
+    "frase18",
+  ];
+
+  var _fraseGerada = "Clique abaixo parar gerar uma nova frase";
+
+  void gerarFrase(){
+    var rng = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[rng];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner:false,
       home: Scaffold(
         appBar:AppBar(
-            title: Text('Random Phrases'),
+            title: Text('Frases do dia'),
           ),
         body: Container(
           child: Row(
@@ -26,25 +59,24 @@ class _HomePageState extends State<HomePage> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: [
                       Container(
-                        padding: EdgeInsets.only(bottom: 70),
-                        decoration: BoxDecoration(
-
-                        ),
                         child: Image.asset('lib/assets/imgs/logo.png'),
                       ),
                       Text(
-                        'alkjdlsajkdlasjdlakjsdpasjdçlaksjdçlaljsdçlajsdlk jasdklj sdaskdlasd jlasdlaskjdaskd aj sdaj s dkjasldjas dlaslalkjdlsajkdlasjdlakjsdpasjdçlaksjdçlaljsdçlajsdlk jasdklj sdaskdlasd jlasdlaskjdaskd aj sdaj s dkjasldjas dlaslalkjdlsajkdlasjdlakjsdpasjdçlaksjdçlaljsdçlajsdlk jasdklj sdaskdlasd jlasdlaskjdaskd aj sdaj s dkjasldjas dlasl',
+                        _fraseGerada,
                         style: TextStyle(
-                          fontSize: 24
+                          fontSize: 17
                         ),
                       ),
-                      TextButton(
-                        onPressed: ()=>{},
+                      ElevatedButton(
+                        onPressed: ()=>{
+                          gerarFrase()
+                        },
                         child: Text(
                             'Nova Frase',
                           style: TextStyle(
                             fontSize: 20,
                             color: Colors.white,
+                            fontWeight: FontWeight.bold
                           ),
                         ),
                         style: ButtonStyle(
